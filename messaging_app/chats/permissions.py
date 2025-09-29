705a07ad7495453e11518e1f5c027a02f7eee38a
Ndata <wisdomndata@gmail.com>
2025-09-30 00:36:14 +0100

    ProDev Backend

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,9 +1,20 @@
 from rest_framework import permissions
+from .models import Conversation, Message
 
 class IsOwner(permissions.BasePermission):
     """
     Custom permission to only allow users to access their own messages/conversations
     """
 
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        """
+        obj can be either a Conversation or a Message.
+        Check if the user is part of the conversation participants.
+        """
+        if isinstance(obj, Conversation):
+            return request.user in obj.participants.all()
+        elif isinstance(obj, Message):
+            return request.user in obj.conversation.participants.all()
+        return False
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsParticipantOfConversation
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
