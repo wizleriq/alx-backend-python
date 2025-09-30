007ca4c734976fa7531c7f59ea3c6d8e844b8e7c
Ndata <wisdomndata@gmail.com>
2025-09-30 22:41:11 +0100

    ProDev Backend Project

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,9 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
-from .permissions import IsParticipantOfConversation
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-
+from .permissions import IsParticipantOfConversation
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
@@ -15,7 +15,7 @@ class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
-def get_queryset(self):
+    def get_queryset(self):
         # Allow filtering by conversation_id
         conversation_id = self.request.query_params.get("conversation_id")
         if conversation_id:
@@ -35,6 +35,20 @@ def get_queryset(self):
 
         # Save the message with sender and conversation
         serializer.save(sender=self.request.user, conversation=conversation)
+
+
+
+
+
+
+
+# from rest_framework import viewsets
+# from rest_framework.permissions import IsAuthenticated
+# from .permissions import IsParticipantOfConversation
+# from .models import Conversation, Message
+# from .serializers import ConversationSerializer, MessageSerializer
+
+
 # class ConversationViewSet(viewsets.ModelViewSet):
 #     queryset = Conversation.objects.all()
 #     serializer_class = ConversationSerializer
@@ -42,6 +56,36 @@ def get_queryset(self):
 
 
 # class MessageViewSet(viewsets.ModelViewSet):
-#     queryset = Message.objects.all()
 #     serializer_class = MessageSerializer
 #     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+# def get_queryset(self):
+#         # Allow filtering by conversation_id
+#         conversation_id = self.request.query_params.get("conversation_id")
+#         if conversation_id:
+#             return Message.objects.filter(conversation_id=conversation_id)
+#         return Message.objects.all()
+
+#     def perform_create(self, serializer):
+#         conversation_id = self.request.data.get("conversation_id")
+#         conversation = Conversation.objects.filter(id=conversation_id).first()
+
+#         # Ensure the user is part of the conversation
+#         if not conversation or self.request.user not in conversation.participants.all():
+#             return Response(
+#                 {"detail": "You are not allowed to send a message in this conversation."},
+#                 status=status.HTTP_403_FORBIDDEN
+#             )
+
+#         # Save the message with sender and conversation
+#         serializer.save(sender=self.request.user, conversation=conversation)
+# # class ConversationViewSet(viewsets.ModelViewSet):
+# #     queryset = Conversation.objects.all()
+# #     serializer_class = ConversationSerializer
+# #     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
+
+# # class MessageViewSet(viewsets.ModelViewSet):
+# #     queryset = Message.objects.all()
+# #     serializer_class = MessageSerializer
+# #     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -118,14 +118,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ),
+    )
 }
+
 
 
 
