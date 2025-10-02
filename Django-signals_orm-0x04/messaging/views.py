1227ff76c2bacab7ebf93c37a7131192599a1dc7
Ndata <wisdomndata@gmail.com>
2025-10-03 00:54:46 +0100

    ORM & Advanced ORM Techniques

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
+from django.shortcuts import render
+from .models import Message
+
+
 
 @login_required
 def delete_user(request):
@@ -11,3 +15,14 @@ def delete_user(request):
     user = request.user
     user.delete()   # This will trigger post_delete signal
     return redirect("/")  # redirect to home or login page
+
+
+def conversation_view(request, user_id):
+    # Fetch top-level messages with related sender, receiver, and replies
+    messages = (
+        Message.objects.filter(receiver_id=user_id, parent_message__isnull=True)
+        .select_related("sender", "receiver")
+        .prefetch_related("replies__sender", "replies__receiver")
+        .order_by("timestamp")
+    )
+    return render(request, "messaging/conversation.html", {"messages": messages})
