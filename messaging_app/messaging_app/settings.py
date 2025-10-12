1d73efeceba7cbefda69d28b8cf03c162d0740af
Ndata <wisdomndata@gmail.com>
2025-10-12 15:27:13 +0100

    Docker

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +75,21 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('MYSQL_DATABASE', default='messaging_db'),
+        'USER': config('MYSQL_USER', default='messaging_user'),
+        'PASSWORD': config('MYSQL_PASSWORD', default='strongpassword123'),
+        'HOST': config('MYSQL_HOST', default='db'),
+        'PORT': config('MYSQL_PORT', default='3306'),
     }
 }
 
